cde6f75f4daf895c6fc720e7499622679aef5e01
pkmitl205 <58030205@kmitl.ac.th>
2017-11-03 13:09:34 +0700

    การทดลองที่ 3 เรื่อง การเขียนโปรแกรม และการใช้เหตุการณ์ของวัตถุเบื้องต้น

---
+++ MultiplicationTable/MultiplicationTable/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub calMultipleTable()
+        Dim a As Integer
+        Dim b As Integer = NumericUpDown1.Value
+        Dim c As Integer
+        Dim tmpStr As String = ""
+        For a = 1 To 12
+            c = a * b
+            tmpStr = tmpStr & b.ToString & " X " & a.ToString & "=" & c.ToString & vbCrLf
+        Next
+        Label1.Text = tmpStr
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        calMultipleTable()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        calMultipleTable()
+    End Sub
+End Class
