baa936ff50e91529df11056e379f2aab1af9692e
pkmitl205 <58030205@kmitl.ac.th>
2017-11-03 17:05:10 +0700

    คำถามท้ายการทดลองที่ 2

--- FrmHelloWorld/FrmHelloWorld/Form1.vb
+++ FrmHelloWorld/FrmHelloWorld/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Private Sub btnHelloWorld_Click(sender As Object, e As EventArgs) Handles btnHelloWorld.Click
-        MessageBox.Show("Hello World")
+        MessageBox.Show("Hello World", "Geeting",
+                        MessageBoxButtons.OKCancel,
+                        MessageBoxIcon.Information)
     End Sub
 End Class
