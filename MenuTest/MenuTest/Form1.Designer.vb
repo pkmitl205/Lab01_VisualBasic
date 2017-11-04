5fc86b1c650bd7b924f0341de9c86803879c1858
pkmitl205 <58030205@kmitl.ac.th>
2017-11-05 01:49:19 +0700

    1.โปรแกรมเพื่อคิดคำนวณค่าพลังงานไฟฟ้า

--- MenuTest/MenuTest/Form1.Designer.vb
+++ MenuTest/MenuTest/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,68 +20,94 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.LoadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.Form2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.Form3ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
-        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(239, 59)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(77, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "คำนวณ"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(39, 59)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(180, 31)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Yellow
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label1.Location = New System.Drawing.Point(39, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(180, 30)
+        Me.Label1.TabIndex = 2
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(239, 113)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(77, 35)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "ออก"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
-        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Size = New System.Drawing.Size(344, 24)
+        Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'LoadToolStripMenuItem
+        'AboutToolStripMenuItem
         '
-        Me.LoadToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Form2ToolStripMenuItem, Me.Form3ToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
-        Me.LoadToolStripMenuItem.Name = "LoadToolStripMenuItem"
-        Me.LoadToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
-        Me.LoadToolStripMenuItem.Text = "Load"
+        Me.AboutToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem1})
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.AboutToolStripMenuItem.Text = "Help"
         '
-        'Form2ToolStripMenuItem
+        'AboutToolStripMenuItem1
         '
-        Me.Form2ToolStripMenuItem.Image = CType(resources.GetObject("Form2ToolStripMenuItem.Image"), System.Drawing.Image)
-        Me.Form2ToolStripMenuItem.Name = "Form2ToolStripMenuItem"
-        Me.Form2ToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+1"
-        Me.Form2ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.Form2ToolStripMenuItem.Text = "Form2"
-        '
-        'Form3ToolStripMenuItem
-        '
-        Me.Form3ToolStripMenuItem.Name = "Form3ToolStripMenuItem"
-        Me.Form3ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.Form3ToolStripMenuItem.Text = "Form3"
-        '
-        'ToolStripMenuItem1
-        '
-        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(149, 6)
-        '
-        'ExitToolStripMenuItem
-        '
-        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.ExitToolStripMenuItem.Text = "Exit"
+        Me.AboutToolStripMenuItem1.Name = "AboutToolStripMenuItem1"
+        Me.AboutToolStripMenuItem1.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem1.Text = "About"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(344, 193)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "โปรแกรมเพื่อคิดคำนวณค่าพลังงานไฟฟ้า "
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
@@ -89,10 +115,11 @@ Partial Class Form1
 
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
     Friend WithEvents MenuStrip1 As MenuStrip
-    Friend WithEvents LoadToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents Form2ToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents Form3ToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
-    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem1 As ToolStripMenuItem
 End Class
--- MenuTest/MenuTest/Form1.vb
+++ MenuTest/MenuTest/Form1.vb
@@ -1,16 +1,40 @@
-﻿Public Class Form1
-    Private Sub Form2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Form2ToolStripMenuItem.Click
-        Dim frm As Form2 = New Form2
-        frm.Show()
+﻿Option Explicit On
+Option Strict On
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim btn_Cal As Integer
+        Dim lbl_result As String
+        btn_Cal = CInt(TextBox1.Text)
+
+        If (Val(TextBox1.Text) >= 1) And (Val(TextBox1.Text) <= 15) Then
+            lbl_result = CType((Val(TextBox1.Text) * 2), String)
+        ElseIf (Val(TextBox1.Text) >= 16) And (Val(TextBox1.Text) <= 25) Then
+            lbl_result = CType((Val(TextBox1.Text) * 3), String)
+        ElseIf (Val(TextBox1.Text) >= 26) And (Val(TextBox1.Text) <= 35) Then
+            lbl_result = CType((Val(TextBox1.Text) * 4), String)
+        ElseIf (Val(TextBox1.Text) >= 36) And (Val(TextBox1.Text) <= 100) Then
+            lbl_result = CType((Val(TextBox1.Text) * 5), String)
+        ElseIf (Val(TextBox1.Text) > 100) Then
+            lbl_result = CType((Val(TextBox1.Text) * 6), String)
+        End If
+#Disable Warning BC42104 ' Variable is used before it has been assigned a value
+        Label1.Text = lbl_result
+#Enable Warning BC42104 ' Variable is used before it has been assigned a value
     End Sub
 
-    Private Sub Form3oolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Form3ToolStripMenuItem.Click
-        Dim frm As Form3 = New Form3
-        frm.Show()
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
     End Sub
 
-    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
-        End
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If IsNumeric(TextBox1.Text) = False And TextBox1.Text <> "" Then
+            MessageBox.Show("ป้อนข้อมูลเฉพาะตัวเลขเท่านั้น.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            TextBox1.Text = ""
+        End If
     End Sub
 
+    Private Sub AboutToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem1.Click
+        MessageBox.Show("สร้างโดย นายภากร รุจิเรขเสรีกุล 58030205", "About")
+    End Sub
 End Class
